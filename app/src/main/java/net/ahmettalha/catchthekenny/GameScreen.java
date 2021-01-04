fab6f62d3d230d24baf4374208fa4ed02998c47a
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2021-01-04 19:19:11 +0300

    Score field has been added.

--- app/src/main/java/net/ahmettalha/catchthekenny/GameScreen.java
+++ app/src/main/java/net/ahmettalha/catchthekenny/GameScreen.java
@@ -3,12 +3,30 @@ package net.ahmettalha.catchthekenny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
-
+    TextView textViewScore;
+    TextView textViewTime;
+    int score;
+    int time; //type = millis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        textViewTime = findViewById(R.id.textViewTime);
+        textViewScore = findViewById(R.id.textViewScore);
+
+        score = 0;
+        time = 15000;
+        textViewScore.setText("Score: " + score);
+        textViewTime.setText("Time: " + time / 1000);
+    }
+
+    public void kennyClick(View view){
+        score++;
+        textViewScore.setText("Score: " + score);
     }
 }
